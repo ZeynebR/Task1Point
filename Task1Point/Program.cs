40aba95d7ff1a46127ebe98bd2e6f2eeb9984d6c
ZeynebR <zeynabnr@code.edu.az>
2023-10-09 08:32:52 +0200

    task1 added

---
+++ Task1Point/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Enter your exam point ");
+int Point = Convert.ToInt32(Console.ReadLine()); 
+
+
+    if (Point >= 0 && Point <= 100)
+    {
+        if (Point >= 91 && Point <= 100)
+        {
+            Console.WriteLine("A");
+        }
+        if (Point >= 81 && Point <= 90)
+        {
+            Console.WriteLine("B");
+        }
+        if (Point >= 71 && Point <= 80)
+        {
+            Console.WriteLine("C");
+        }
+        if (Point >= 61 && Point <= 70)
+        {
+            Console.WriteLine("D");
+        }
+        if (Point >= 51 && Point <= 60)
+        {
+            Console.WriteLine("E");
+        }
+        if (Point < 51)
+        {
+            Console.WriteLine("You failed the exam");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Point should be a number between 1 and 100");
+    }
